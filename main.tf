7303bc8f54cd0d793d1211231051b6b7169b388c
Kamsani1973 <sanib@live.com>
2025-07-10 21:43:07 +0800

    Modified Main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "kamsani"  #Change this
+    key    = "kamsani" #Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "kamsani-Assignment3.2"  # Set your bucket name here
+  bucket_prefix = "kamsani-Assignment3.2" # Set your bucket name here
 }
